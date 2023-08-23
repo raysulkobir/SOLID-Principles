4fb76ad320da05e5353f39342d6133e4004af954
Md.Raysu kobir <86109661+raysulkobir@users.noreply.github.com>
2023-08-23 17:58:10 +0600

    single responsiblity into add

--- Single responsiblity/index.php
+++ Single responsiblity/index.php
@@ -1,21 +1,45 @@
-<?php 
-    include "inc/header.php";
-    include "inc/database.php";
-    include "inc/student.php";
-?>
-<?php
-//       function __autoload($calss_name){
-//           include "inc/".$calss_name.".php";
-//       }
-?>
+<?php include "inc/header.php" ?>
 
-    <div class="para">
-        <?php
-           $db = new database();
-           $db = new database();
-         ?>
-        
-     
-     </div>
+As the name suggests, this principle states that each class should have one responsibility, one single purpose. This means that a class will do only one job, which leads us to conclude it should have only one reason to change.
 
-<?php include "inc/footer.php"; ?>
+We don’t want objects that know too much and have unrelated behavior. These classes are harder to maintain. For example, if we have a class that we change a lot, and for different reasons, then this class should be broken down into more classes, each handling a single concern. Surely, if an error occurs, it will be easier to find.
+<hr>
+<b>SRP = Single Responsibility Principle</b>
+    <h1>S – Single Responsibility Principle</h1>
+
+
+    <?php
+    class Student
+    {
+        function GradeCalculator()
+        {
+        }
+        function Attendance()
+        {
+        }
+        function StudentPayment()
+        {
+        }
+    }
+
+    //* This is single  design  pattern 
+
+    class GradeCalculator
+    {
+    }
+
+    class Attendance
+    {
+    }
+
+    class StudentPayment
+    {
+    }
+
+
+
+
+    ?>
+
+
+    <?php include "inc/footer.php"; ?>
